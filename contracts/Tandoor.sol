f15e34f7ef4c46c12b4a0ff72a085f04fdc467f7
5r33n <5r33ns@gmail.com>
2023-01-30 19:46:55 +0000

    fixed dynamic arrays and added getProblem to retrieve corr IOs

--- contracts/Tandoor.sol
+++ contracts/Tandoor.sol
@@ -10,13 +10,18 @@ pragma solidity ^0.8.17;
 contract Tandoor {
     struct Problem {
         uint256 id;
-        uint256[3] inputs;
-        uint256[3] outputs;
+        uint256[] inputs;
+        uint256[] outputs;
     }
 
     Problem[] public problems;
 
-    function setProblem(uint256[3] memory _inputs, uint256[3] memory _outputs) public {
+    function setProblem(uint256[] memory _inputs, uint256[] memory _outputs) public {
         problems.push(Problem(problems.length, _inputs, _outputs));
+    }
+
+    function getProblem(uint256 _id) public view returns (uint256[] memory, uint256[] memory) {
+        Problem storage problem = problems[_id];
+        return (problem.inputs, problem.outputs);
     }
 }
