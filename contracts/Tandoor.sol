dbad3aa014a8b3f24bb0a7b120d5e9e82b5e65aa
5r33n <5r33ns@gmail.com>
2023-01-31 11:19:44 +0000

    added reward to setProblem func and added an example dir for solvers

--- contracts/Tandoor.sol
+++ contracts/Tandoor.sol
@@ -8,16 +8,24 @@
 pragma solidity ^0.8.17;
 
 contract Tandoor {
+    event ProblemEvent(uint256 indexed id, uint256 reward);
+
     struct Problem {
         uint256 id;
+        uint256 reward;
         uint256[] inputs;
         uint256[] outputs;
     }
 
     Problem[] public problems;
 
-    function setProblem(uint256[] memory _inputs, uint256[] memory _outputs) public {
-        problems.push(Problem(problems.length, _inputs, _outputs));
+    function setProblem(
+        uint256 _reward,
+        uint256[] memory _inputs,
+        uint256[] memory _outputs
+    ) public {
+        problems.push(Problem(problems.length, _reward, _inputs, _outputs));
+        emit ProblemEvent(problems.length - 1, _reward);
     }
 
     function getProblem(uint256 _id) public view returns (uint256[] memory, uint256[] memory) {
