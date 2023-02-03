a5d2bca1a3c044aca54ac5b940fbe1d41bd9aacf
5r33n <5r33ns@gmail.com>
2023-02-03 13:29:09 +0000

    added encryption/decryption functions

---
+++ scripts/crypto.ts
@@ -0,0 +1,30 @@
+import sjcl from "sjcl"
+
+interface Io {
+  input: number
+  output: number
+}
+
+const sampleIOList: Io = {
+  1: 2,
+  2: 3,
+  3: 4,
+  4: 5,
+  5: 6,
+}
+
+const encrypt = (_pwd, _ioList) => {
+  const jsonedIoList = JSON.stringify(_ioList)
+  let encrypted = sjcl.json.encrypt(_pwd, jsonedIoList)
+  return encrypted
+}
+
+const decrypt = (_pwd, _ioList) => {
+  let decrypted = sjcl.json.decrypt("pwd", encrypted)
+  return decrypted
+}
+
+//const encrypted = encrypt("pwd", sampleIOList)
+//const decrypted = decrypt("pwd", encrypted)
+
+export { Io, encrypt, decrypt }
