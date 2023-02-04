48f2ae7823cea3352bdeffa11d91b7dad24572e2
5r33n <5r33ns@gmail.com>
2023-02-04 13:45:26 +0000

    building ipfs tools

--- scripts/ipfs.ts
+++ scripts/ipfs.ts
@@ -1,5 +1,5 @@
 import { Io, encrypt, decrypt } from "./crypto"
-import { Web3Storage, getFilesFromPath } from "web3.storage"
+import { Web3Storage, getFilesFromPath, File } from "web3.storage"
 
 const sampleIOList: Io = {
   1: 2,
@@ -15,10 +15,45 @@ const decrypted = decrypt("pwd", encrypted)
 const token = process.env.WEB3STORAGE_TOKEN
 const client = new Web3Storage({ token })
 
-const storeFiles = async () => {
-  const files = await getFilesFromPath("./files-tmp")
-  const cid = await client.put(files)
-  console.log(cid)
+const setFiles = () => {
+  const obj = { hello: "woddrld" }
+  const buffer = Buffer.from(JSON.stringify(obj))
+  const files = [new File([buffer], "hello.json")]
+  return files
+  //const files = await getFilesFromPath("./scripts/files-tmp")
+  //console.log(files)
+  //await client.put(files)
+  //console.log(cid)
 }
 
-storeFiles()
+const getFiles = async () => {
+  const cid = "QmZnjog5L9DheF5egGVTuCxcu7csU858eupUkpHVSZ3Yuq"
+  const res = await client.get(cid)
+  const files = await res.files()
+
+  for (const file of files) {
+    console.log(`cid: ${file.cid}`)
+    console.log(`name: ${file.name}`)
+    console.log(`size: ${file.size} bytes`)
+  }
+}
+
+async function storeFiles(files) {
+  const cid = await client.put(files, { onRootCidReady, onStoredChunk })
+  console.log("stored files with cid:", cid)
+  return cid
+}
+
+const onRootCidReady = (cid) => {
+  console.log("uploading files with cid:", cid)
+}
+
+const onStoredChunk = (size) => {
+  uploaded += size
+  const pct = totalSize / uploaded
+  console.log(`Uploading... ${pct.toFixed(2)}% complete`)
+}
+
+const files = setFiles()
+storeFiles(files)
+//getFiles()
